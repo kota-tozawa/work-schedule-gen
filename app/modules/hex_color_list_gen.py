5e3f741d02ec05a42943c692110ee2b4c49936a6
“Kota <tozawakota@gmail.com>
2020-10-01 08:33:21 +0900

    update hex_color_list_gen.py, hex_color_list.xls

--- app/modules/hex_color_list_gen.py
+++ app/modules/hex_color_list_gen.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from xlwt import Workbook, Pattern, XFStyle
 
+# xlwtで利用可能な全ての色をxlsファイル上で表示する
+# （参考：https://github.com/python-excel/xlwt/blob/master/xlwt/Style.py）
 # TODO 関数「to_hex_int()」はここでしか使わないであろうから、utilsではなく一旦ここに置いておく
 
 
@@ -13,7 +15,7 @@ def to_hex_int(hex_str: str) -> int:
 # xlwtで利用可能な色を一括表示する用のスタイル
 style_test_list = []
 hex_list = []
-# 0x0(0)から0xfe(256)までの色
+# 0x0(0)から0xff(256)までの色
 for i in range(256):
     hex_str = hex(i)
     hex_int = to_hex_int(hex_str)
@@ -31,11 +33,12 @@ wb_test = Workbook()
 ws_test = wb_test.add_sheet("256 colors available in xlwt")
 
 # 色をセルの背景色にセット
+ws_test.write(0, 0, "xlwtで利用可能な色の一覧")
 for i in range(256):
     text = 'decimal: {dec}, hexadecimal: {hex}'.format(dec=str(i), hex=hex(i))
-    ws_test.write(i, 0, text, style_test_list[i])
+    ws_test.write(i + 1, 0, text, style_test_list[i])
 
-# 256色入りxlsファイルをtemplateディレクトリに保存
+# xlsファイルをtemplateディレクトリに保存
 file_name = 'hex_color_list.xls'
 file_path = str(Path('../template') / file_name)
 wb_test.save(file_path)
