25951a7b590fab709ea7e878ce84fab6baf90091
“Kota <tozawakota@gmail.com>
2020-09-30 20:38:14 +0900

    update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#
+# work-schedule-genディレクトリ以下（appディレクトリと同じ階層）をカレントディレクトリにしてから実行
+#
 
-echo 'コードを整形中...'
+# 1行文字数制限は無視（F401,E501）
+echo "全てのPythonスクリプトを整形中..."
 autopep8 --in-place --aggressive --ignore errors=E226,E24,W50,W690,F401,E501 app/*.py
 autopep8 --in-place --aggressive --ignore errors=E226,E24,W50,W690,F401,E501 app/modules/*.py
 
-echo 'Pythonスクリプトのフォーマットの問題点を検出中...'
+echo "全てのPythonスクリプトのフォーマットの問題点を検出中..."
 result=`flake8 --extend-ignore=F401,E501 app/`
-if ["$result" -eq '']
+if ["$result" -eq ""]
 then
-  echo '問題点は見つかりませんでした。'
+  echo "問題点は見つかりませんでした。"
 else
   echo "$result"
 fi
