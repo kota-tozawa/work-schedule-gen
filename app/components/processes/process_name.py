65fb3a2020004220b44435cb6ba5df45c35b8a14
“Kota <tozawakota@gmail.com>
2020-10-02 03:11:57 +0900

    fix process_name.py

--- app/components/processes/process_name.py
+++ app/components/processes/process_name.py
@@ -5,6 +5,6 @@ from components.organisms.get_name import get_last_or_first_name
 def process_name() -> Tuple[str, str, str]:
     last_name = get_last_or_first_name()
     first_name = get_last_or_first_name(last_name=False)
-    full_name = first_name + last_name
+    full_name = last_name + first_name
 
-    return first_name, last_name, full_name
+    return last_name, first_name, full_name
