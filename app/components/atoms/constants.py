e4536775cb974097f63cf637ed18ff97753db9a5
“Kota <tozawakota@gmail.com>
2020-10-02 09:24:32 +0900

    change dir name: template -> templates.

--- app/components/atoms/constants.py
+++ app/components/atoms/constants.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 
 WEEK_DAYS = ['月', '火', '水', '木', '金', '土', '日']
-TEMPLATE_FILE_PATH = str(Path('./template/template.xls'))
+TEMPLATE_FILE_PATH = str(Path('./templates/template.xls'))
