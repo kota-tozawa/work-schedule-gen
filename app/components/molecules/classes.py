a3f9d260a74ba9cb01224d3c7639e9cf3a570e46
“Kota <tozawakota@gmail.com>
2020-10-10 14:56:24 +0900

    apply property decorator to Date class.

--- app/components/molecules/classes.py
+++ app/components/molecules/classes.py
@@ -1,23 +1,37 @@
 class Date:
     def __init__(self, day, week_day, holiday_flg):
-        self.day = day
+        self.day = day if day > 0 else 0
         self.week_day = week_day
         self.holiday_flg = holiday_flg
 
-    def set_day(self, day):
-        self.day = day
+    # getters
+    @property
+    def day(self):
+        return self.__day
 
-    def set_week_day(self, week_day):
-        self.week_day = week_day
+    @property
+    def week_day(self):
+        return self.__week_day
 
-    def set_holiday_flg(self, holiday_flg):
-        self.holiday_flg = holiday_flg
+    @property
+    def holiday_flg(self):
+        return self.__holiday_flg
 
-    def get_day(self):
-        return self.day
+    # setters
+    @day.setter
+    def day(self, day):
+        if day is None:
+            raise TypeError('不正な日付です。')
+        self.__day = day
 
-    def get_week_day(self):
-        return self.week_day
+    @week_day.setter
+    def week_day(self, week_day):
+        if week_day is None:
+            raise TypeError('不正な曜日です。')
+        self.__week_day = week_day
 
-    def get_holiday_flg(self):
-        return self.holiday_flg
+    @holiday_flg.setter
+    def holiday_flg(self, holiday_flg):
+        if holiday_flg is None:
+            raise TypeError('不正な休日・祝日判定です。')
+        self.__holiday_flg = holiday_flg
