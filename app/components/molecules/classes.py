73828cb3f5c279e595bce0efb24f82cf1c38aed4
“Kota <tozawakota@gmail.com>
2020-10-11 10:58:03 +0900

    update test_classes.py

--- app/components/molecules/classes.py
+++ app/components/molecules/classes.py
@@ -1,6 +1,22 @@
+from components.atoms.constants import WEEK_DAYS
+from components.molecules.exceptions import NonExistentValueError
+
+
 class Date:
     def __init__(self, day: int, week_day: str, holiday_flg: bool):
-        self.day = day if day > 0 else 0
+        if day is None:
+            raise TypeError('日付がありません。')
+        if week_day is None or '':
+            raise TypeError('曜日がありません。')
+        if holiday_flg is None:
+            raise TypeError('休日・祝日判定がありません。')
+
+        if day < 1 or day > 31:
+            raise NonExistentValueError()
+        if week_day not in WEEK_DAYS:
+            raise NonExistentValueError()
+
+        self.day = day
         self.week_day = week_day
         self.holiday_flg = holiday_flg
 
@@ -20,18 +36,12 @@ class Date:
     # setters
     @day.setter
     def day(self, day: int):
-        if day is None:
-            raise TypeError('不正な日付です。')
         self.__day = day
 
     @week_day.setter
     def week_day(self, week_day: str):
-        if week_day is None or '':
-            raise TypeError('不正な曜日です。')
         self.__week_day = week_day
 
     @holiday_flg.setter
     def holiday_flg(self, holiday_flg: bool):
-        if holiday_flg is None:
-            raise TypeError('不正な休日・祝日判定です。')
         self.__holiday_flg = holiday_flg
--- app/components/molecules/exceptions.py
+++ app/components/molecules/exceptions.py
@@ -6,3 +6,8 @@ class TooLongStringError(Exception):
 class TooShortStringError(Exception):
     def __str__(self):
         return '引数の文字列が短すぎます。'
+
+
+class NonExistentValueError(Exception):
+    def __str__(self):
+        return '存在し得ない値です。'
